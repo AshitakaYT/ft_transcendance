8923b27b20e2ad03421546dd4b7d35af6a13e9ac
iel-amra <ihab-17@hotmail.com>
2023-12-07 13:35:31 +0100

    New collision system

--- srcs/game/views.py
+++ srcs/game/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    f = open(os.getcwd() + "/game/test.html", "r")
-    return HttpResponse(f.read())
+    #f = open(os.getcwd() + "/game/test.html", "r")
+    template = loader.get_template("game/game.html")
+    context = {
+        "width_canvas": 1020,
+        "height_canvas": 620,
+        "width": 100,
+        "height": 600,
+        "paddleh" : 200,
+        "paddlew" : 20,
+        "paddled" : 20
+    }
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse(f.read())
