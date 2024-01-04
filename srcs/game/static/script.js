a5370055a2ff1f991f5af22a74dc8fd7af27dfad
nchabli <87837544+Elsruora@users.noreply.github.com>
2024-01-04 01:18:51 +0100

    added few basics graphics + start/pause functionnality

---
+++ srcs/game/static/script.js
@@ -0,0 +1,552 @@
+let PI = Math.PI;
+
+    //document.getElementById("test").innerHTML = Width;
+    //document.getElementById("circle").setAttribute("height", string(Height + 20));
+
+    document.getElementById('startButton').addEventListener('click', startGame);
+    document.getElementById('pauseButton').addEventListener('click', pauseGame);
+
+    let gameRunning = false;
+    let gameAnimationFrame;
+
+function startGame() {
+    if (!gameRunning) {
+        gameRunning = true;
+        gameAnimationFrame = requestAnimationFrame(frame);
+    }
+}
+
+function pauseGame() {
+    if (gameRunning) {
+        gameRunning = false;
+        cancelAnimationFrame(gameAnimationFrame);
+    }
+}
+
+    var data =
+    {
+        t : 0.1,
+        up : false,
+        down : false,
+        ball :
+        {
+            x : 0,
+            y : 0,
+            r : 25,
+            rv : ballv,
+            tv : 0,
+        },
+        paddleR :
+        {
+            x : width / 2 - paddled - paddlew,
+            y : - paddleh / 2,
+            vy : 0,
+            recover : 1,
+            score : 0
+        },
+        paddleL :
+        {
+            x : paddled - width / 2,
+            y : - paddleh / 2,
+            vy : 0,
+            recover : 1,
+            score : 0
+        }
+    };
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key == 's' && !e.repeat)
+            data.up = true;
+        if (e.key == 'w' && !e.repeat)
+            data.down = true;
+    });
+
+    document.addEventListener("keyup", (e) => {
+            if (e.key == 's' && !e.repeat)
+                data.up = false;
+            if (e.key == 'w' && !e.repeat)
+                data.down = false;
+        });
+
+    
+    function frame()
+    {
+
+        if (!simulate())
+        {
+            //draw();
+            animate();
+            if (gameRunning == true)
+                window.requestAnimationFrame(frame);
+        }
+        //setInterval(frame(), 20);
+
+    }
+
+    function edgesColl(tr)
+    {
+        if (data.ball.x + ballvx(data.ball) * tr.dt < data.ball.r - width / 2 - lengoal)
+        {            
+            tr.ntv = 0;
+            tr.nrv = ballv;
+            tr.restart = true;
+            // tr.ntv = sanangle(PI - data.ball.tv);
+            tr.dt = (data.ball.r - width / 2 - lengoal - data.ball.x) / ballvx(data.ball)
+        }
+        if (data.ball.x + ballvx(data.ball) * tr.dt > width / 2 - data.ball.r + lengoal)
+        {
+            tr.ntv = PI;
+            tr.nrv = ballv;
+            tr.restart = true;
+            // tr.ntv = sanangle(PI - data.ball.tv);
+            tr.dt = (width / 2 - data.ball.r  + lengoal - data.ball.x) / ballvx(data.ball)
+        }
+
+        if (data.ball.y + ballvy(data.ball) * tr.dt < data.ball.r - height / 2 && Math.sin(data.ball.tv) < 0)
+        {
+            tr.ntv = sanangle(- data.ball.tv);
+            tr.dt = (data.ball.r - height / 2 - data.ball.y) / ballvy(data.ball)
+        }
+        if (data.ball.y + ballvy(data.ball) * tr.dt > height / 2 - data.ball.r && Math.sin(data.ball.tv) > 0)
+        {
+            tr.ntv = sanangle(- data.ball.tv);
+            tr.dt = (height / 2 - data.ball.r - data.ball.y) / ballvy(data.ball)
+        }
+        return (tr);
+    }
+
+    function paddlesColl(tr)
+    {
+        var dtc;
+        var paddle;
+        var side;
+
+        for (let i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    side = 1;
+                    paddle = data.paddleL;
+                    dtc = (data.paddleL.x + paddlew - data.ball.x + data.ball.r) / ballvx(data.ball);
+                    break;
+                case 1:
+                    side = - 1;
+                    dtc = (data.paddleL.x - data.ball.x - data.ball.r) / ballvx(data.ball);
+                    break;
+                case 2:
+                    side = 1;
+                    paddle = data.paddleR;
+                    dtc = (data.paddleR.x + paddlew - data.ball.x + data.ball.r) / ballvx(data.ball);
+                    break;
+                case 3:
+                    side = -1;
+                    dtc = (data.paddleR.x - data.ball.x - data.ball.r) / ballvx(data.ball);
+                    break;
+            }
+            if (dtc >= 0 && dtc <= tr.dt && data.ball.y + ballvy(data.ball) * dtc > paddle.y + paddle.vy * dtc 
+                && data.ball.y + ballvy(data.ball) * dtc < paddle.y + paddle.vy * dtc + paddleh
+                && Math.cos(data.ball.tv) * side < 0)
+            {
+                tr.rv = data.ball.rv / ballfriction;
+                tr.dt = dtc;
+                tr.ntv = sanangle(((2 * (data.ball.y + ballvy(data.ball) * dtc - (paddle.y + dtc * paddle.vy)) / paddleh) - 1) * PI / 4);
+                if (side == -1)
+                    tr.ntv = sanangle(PI - tr.ntv);
+                //document.getElementById("test").innerHTML = (data.ball.y + ballvy(data.ball) * dtc - (paddle.y + dtc * paddle.vy)) / paddleh;
+            }
+        }
+        return (tr);
+    }
+
+    function spheres(sphere1, sphere2)
+    {
+        let dx = sphere1.x - sphere2.x;
+        let dy = sphere1.y - sphere2.y;
+        let dvx = ballvx(sphere1) - ballvx(sphere2);
+        let dvy = ballvy(sphere1) - ballvy(sphere2);
+
+        let a = Math.pow(dvx, 2) + Math.pow(dvy, 2);
+        if (a == 0)
+            return (-1);
+        let b = 2 * (dx * dvx + dy * dvy);
+        let c = Math.pow(dx, 2) + Math.pow(dy, 2) - Math.pow(sphere1.r + sphere2.r, 2);
+
+        let delta = Math.pow(b, 2) - 4 * a * c;
+
+        if (delta < 0)
+            return (-1);
+        return ((- b - Math.sqrt(delta)) / (2 * a));
+    }
+
+
+    function allSpheresColl(tr)
+    {
+        var coll;
+        var little =
+        {
+            x : data.paddleL.x + paddlew / 2,
+            y : data.paddleL.y,
+            r : paddlew / 2,
+            rv : data.paddleL.vy,
+            tv : PI / 2
+        };
+        coll = spheres(little, data.ball);
+        //document.getElementById("test").innerHTML = coll;
+        //logMessage(coll);
+        if (coll >= 0 && coll <= tr.dt)
+        {
+            tr.nrv *= smash;
+            tr.dt = coll;
+            tr.ntv = sanangle(- PI / 4);
+            tr.nlvy = recoil;
+            data.paddleL.recover = 0;
+        }
+
+        little.y = data.paddleL.y + paddleh;
+
+        coll = spheres(little, data.ball);
+        if (coll >= 0 && coll <= tr.dt)
+        {
+            tr.nrv *= smash;
+            tr.dt = coll;
+            tr.ntv = sanangle(PI / 4);
+            tr.nlvy = - recoil;
+            data.paddleL.recover = 0;
+        }
+
+        little.x = data.paddleR.x + paddlew / 2;
+        little.y = data.paddleR.y;
+        little.rv = data.paddleR.vy;
+
+        coll = spheres(little, data.ball);
+        if (coll >= 0 && coll <= tr.dt)
+        {
+            tr.nrv *= smash;
+            tr.dt = coll;
+            tr.ntv = sanangle(- 3 * PI / 4);
+            tr.nrvy = recoil;
+            data.paddleR.recover = 0;
+        }
+
+        little.y = data.paddleR.y + paddleh;
+
+        coll = spheres(little, data.ball);
+        if (coll >= 0 && coll <= tr.dt)
+        {
+            tr.nrv *= smash;
+            tr.dt = coll;
+            tr.ntv = sanangle(3 * PI / 4);
+            tr.nrvy = - recoil;
+            data.paddleR.recover = 0;
+        }
+        return (tr);
+    }
+
+    function paddles(tr)
+    {
+        if (data.paddleL.y + data.paddleL.vy * tr.dt - paddlew / 2 < - height / 2)
+        {
+            tr.dt = (- height / 2 - data.paddleL.y + paddlew / 2) / data.paddleL.vy;
+            tr.nlvy = 0;
+        }
+        else if (data.paddleL.y + data.paddleL.vy * tr.dt + paddleh + paddlew / 2 > height / 2)
+        {
+            tr.dt = (height / 2 - data.paddleL.y - paddlew / 2 - paddleh) / data.paddleL.vy;
+            tr.nlvy = 0;
+        }
+        if (data.paddleR.y + data.paddleR.vy * tr.dt - paddlew / 2 < - height / 2)
+        {
+            tr.dt = (- height / 2 - data.paddleR.y + paddlew / 2) / data.paddleR.vy;
+            tr.nrvy = 0;
+        }
+        else if (data.paddleR.y + data.paddleR.vy * tr.dt + paddleh + paddlew / 2 > height / 2)
+        {
+            tr.dt = (height / 2 - data.paddleR.y - paddlew / 2 - paddleh) / data.paddleR.vy;
+            tr.nrvy = 0;
+        }
+        return (tr);
+    }
+
+    function input()
+    {
+        data.paddleL.vy /= paddlefriction; // Deplacer pour que ce soit en fonction du t et dans la simulation
+        data.paddleR.vy /= paddlefriction;
+        data.ball.rv /= ballfriction;
+        if (data.ball.rv < ballv)
+            data.ball.rv = ballv;
+        data.paddleL.recover += data.t / recovertime;
+        data.paddleR.recover += data.t / recovertime; 
+        if (data.paddleL.recover > 1)
+            data.paddleL.recover = 1;
+        if (data.paddleR.recover > 1)
+            data.paddleR.recover = 1; //
+        if (data.up == true)
+        {
+            data.paddleL.vy -= paddlespeed * data.paddleL.recover * data.t;
+            data.paddleR.vy -= paddlespeed * data.paddleR.recover * data.t;
+        }
+        if (data.down == true)
+        {
+            data.paddleL.vy += paddlespeed * data.paddleL.recover * data.t;
+            data.paddleR.vy += paddlespeed * data.paddleR.recover * data.t;
+        }
+
+        //IA input
+
+
+    }
+
+    function sanangle(angle) {return angle % (2 * PI)}
+    function ballvx(ball) {return ball.rv * Math.cos(ball.tv);}
+    function ballvy(ball) {return ball.rv * Math.sin(ball.tv);}
+
+    function simulate()
+    {
+        document.getElementById("score").innerHTML = "Score : " + data.paddleL.score + " - " + data.paddleR.score;
+        input();
+        var i = 0;
+        var inter;
+        var final;
+        while (data.t > 0 && i < 10000)
+        {
+            let tr = {
+                dt : data.t,
+                nrv : data.ball.rv,
+                ntv : data.ball.tv,
+                nlvy : data.paddleL.vy,
+                nrvy : data.paddleR.vy,
+                restart : false
+            }
+            final = {...tr};
+            // document.getElementById("test").innerHTML = JSON.stringify(data) + "<br>";
+            // logMessage("final.dt = " + final.dt);
+
+            inter = paddles({...tr});
+            tr.dt = inter.dt;
+            if (inter.dt < final.dt)
+                final = {...inter};
+            inter = edgesColl({...tr});
+            tr.dt = inter.dt;
+            if (inter.dt < final.dt)
+                final = {...inter};
+            inter = paddlesColl({...tr});
+            tr.dt = inter.dt;
+            if (inter.dt < final.dt)
+                final = {...inter};
+            inter = allSpheresColl({...tr});
+            tr.dt = inter.dt;
+            if (inter.dt < final.dt)
+                final = {...inter};
+
+            data.ball.x += ballvx(data.ball) * final.dt;
+            data.ball.y += ballvy(data.ball) * final.dt;
+            if (final.restart)
+            {
+                if (data.ball.x > 0)
+                    data.paddleL.score++;
+                else
+                    data.paddleR.score++;
+                data.ball.x = 0;
+                data.ball.y = 0;
+            }
+            data.paddleL.y += data.paddleL.vy * final.dt;
+            data.paddleR.y += data.paddleR.vy * final.dt;
+
+            data.ball.rv = final.nrv;
+            data.ball.tv = final.ntv;
+            if (data.ball.rv > 1000000)
+                data.ball.rv = 1000000;
+
+            data.paddleL.vy = final.nlvy;
+            data.paddleR.vy = final.nrvy;
+            data.t -= final.dt;
+
+            i++;
+            // logMessage("i = " + i);
+            if (i == 100000)
+            {
+                return (1);
+            }
+       }
+        data.t = 0.1;
+        return (0);
+    }
+
+    function logMessage(message) {
+        document.getElementById('score').innerHTML += `${message}<br>`;
+    }
+
+    // Partie de Julien
+    import * as THREE from 'three';
+    let my=0;
+    let light_pow = 100
+    let fov_deg = 1;
+    let fov_rad = fov_deg * PI/180;
+
+
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0.01,0.01,0.01);
+
+    const camera = new THREE.PerspectiveCamera( fov_deg, window.innerWidth / window.innerHeight, 0.1, 100000 );
+    camera.position.set(0,0,Math.max(width, height)/(2.*Math.tan(fov_rad/2.)));
+    camera.lookAt(0,0,0);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    document.body.appendChild( renderer.domElement );
+    //document.addEventListener("mousemove",mouseevent);
+    window.addEventListener("resize",windowsresize);
+
+    const geometry_pad = new THREE.CapsuleGeometry( paddlew/2., paddleh, 10, 30 );
+    const geometry_ball = new THREE.SphereGeometry( data.ball.r, 10, 30 );
+    const geometry_edge = new THREE.CylinderGeometry( paddlew, paddlew, width*2., 60 );
+    const material_lit = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+    const material_unlit = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+    let arrball = [];
+    let len = 10;
+    arrball.length = len;
+    for (let i=0; i<len; ++i) {
+        arrball[i] = new THREE.Mesh( geometry_ball, material_unlit );
+        arrball[i].scale.set((i+1)/len,(i+1)/len,(i+1)/len);
+        scene.add(arrball[i]);
+    }
+
+    const padl = new THREE.Mesh( geometry_pad, material_lit );
+    scene.add(padl);
+    padl.castShadow = true;
+
+    const padr = new THREE.Mesh( geometry_pad, material_lit );
+    scene.add(padr);
+    padr.castShadow = true;
+
+    const edgeu = new THREE.Mesh( geometry_edge, material_lit );
+    scene.add(edgeu);
+    edgeu.receiveShadow = true;
+    edgeu.rotation.z = 3.1415926535/2;
+    edgeu.position.y = height/2+paddlew;
+
+    const edged = new THREE.Mesh( geometry_edge, material_lit );
+    scene.add(edged);
+    edged.receiveShadow = true;
+    edged.rotation.z = 3.1415926535/2;
+    edged.position.y = -height/2-paddlew;
+
+    const light = new THREE.PointLight(0x0000ff, data.ball.r * data.ball.r * light_pow);
+    light.castShadow=true;
+    scene.add(light);
+
+    const amblight = new THREE.AmbientLight(0xffffff, .01);
+    scene.add(amblight);
+
+    let rot = 0;
+
+
+    function animate() {
+        light.color.offsetHSL(0.003,0,0);
+        material_unlit.color.offsetHSL(0.003,0,0);
+
+        for (let i=0; i<len-1; ++i) {
+            arrball[i].position.set(arrball[i+1].position.x,arrball[i+1].position.y,arrball[i+1].position.z,);
+        }
+        arrball[len-1].position.set(data.ball.x,data.ball.y,0);
+        light.position.set(data.ball.x,data.ball.y,0);
+        padl.position.set(data.paddleL.x + paddlew/2,data.paddleL.y + paddleh/2,0);
+        padr.position.set(data.paddleR.x + paddlew/2,data.paddleR.y + paddleh/2,0);
+
+        rot += 0.002;
+        //camera.position.set(Math.sin(rot)*5,0,Math.cos(rot)*5);
+        //camera.up.set(Math.cos(rot)*5,0,Math.sin(rot)*-5);
+        camera.lookAt(0,0,0);
+
+        renderer.render( scene, camera );
+    }
+
+    function mouseevent(event) {
+        my = event.clientY/window.innerHeight*-2+1;
+    }
+
+    function windowsresize() {
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+
+
+
+    frame();
+
+    // function draw()
+    // {
+    //     var canvas = document.getElementById('game');
+
+    //     if (canvas.getContext)
+    //         {
+    //             var ctx = canvas.getContext('2d'); 
+    //             ctx.clearRect(0, 0, canvas.width , canvas.height);
+                
+    //             // Rectangle game
+
+    //             ctx.beginPath();
+    //             ctx.moveTo((canvas.width - data.w) / 2, (canvas.height - data.h) / 2);
+    //             ctx.lineTo((canvas.width + data.w) / 2 + data.w, (canvas.height - data.h) / 2);
+    //             ctx.stroke();
+
+    //             ctx.beginPath();
+    //             ctx.moveTo((canvas.width - data.w) / 2, (canvas.height + data.h) / 2);
+    //             ctx.lineTo((canvas.width + data.w) / 2, (canvas.height + data.h) / 2);
+    //             ctx.stroke();
+
+    //             // ctx.beginPath();
+    //             // ctx.lineWidth = 3;
+    //             // ctx.rect((canvas.width - data.w) / 2, (canvas.height - data.h) / 2, data.w, data.h);
+    //             // ctx.strokeStyle = '#000000';
+    //             // ctx.stroke();
+                
+    //             //Ball
+
+    //             ctx.beginPath();
+    //             ctx.fillStyle = '#FF0000';
+    //             ctx.arc(data.ball.x + canvas.width / 2, data.ball.y + canvas.height / 2, data.ball.r, 0, 2 * PI, false);
+    //             ctx.fill();
+
+    //             // Left Paddle
+
+    //             ctx.beginPath();
+    //             ctx.fillStyle = '#0000FF';
+    //             ctx.arc((canvas.width + data.paddleL.w) / 2 + data.paddleL.x, canvas.height / 2 + data.paddleL.y, data.paddleL.w / 2, 0, 2 * PI, false);
+    //             ctx.fill();
+
+    //             ctx.beginPath();
+    //             ctx.lineWidth = 3;
+    //             ctx.rect(canvas.width / 2 + data.paddleL.x, canvas.height / 2 + data.paddleL.y, data.paddleL.w, data.paddleL.h);
+    //             ctx.fillStyle = '#0000FF';
+    //             ctx.fill();
+
+    //             ctx.beginPath();
+    //             ctx.fillStyle = '#0000FF';
+    //             ctx.arc((canvas.width + data.paddleL.w) / 2 + data.paddleL.x, canvas.height / 2 + data.paddleL.y + data.paddleL.h, data.paddleL.w / 2, 0, 2 * PI, false);
+    //             ctx.fill();
+
+    //             //Right Paddle
+
+    //             ctx.beginPath();
+    //             ctx.fillStyle = '#0000FF';
+    //             ctx.arc((canvas.width + data.paddleR.w) / 2 + data.paddleR.x, canvas.height / 2 + data.paddleR.y, data.paddleR.w / 2, 0, 2 * PI, false);
+    //             ctx.fill();
+
+    //             ctx.beginPath();
+    //             ctx.lineWidth = 3;
+    //             ctx.rect(canvas.width / 2 + data.paddleR.x, canvas.height / 2 + data.paddleR.y, data.paddleR.w, data.paddleR.h);
+    //             ctx.fillStyle = '#0000FF';
+    //             ctx.fill();
+
+    //             ctx.beginPath();
+    //             ctx.fillStyle = '#0000FF';
+    //             ctx.arc((canvas.width + data.paddleR.w) / 2 + data.paddleR.x, canvas.height / 2 + data.paddleR.y + data.paddleR.h, data.paddleR.w / 2, 0, 2 * PI, false);
+    //             ctx.fill();
+    //         }
+    // }
